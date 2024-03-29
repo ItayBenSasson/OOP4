5dcd24399ff9d23264ab6875c72b0773f67878f2
Yonatan Reicher <yony252525@gmail.com>
2024-03-29 13:56:55 +0300

    Fix StoryTestExceptionImpl which was merged incorrectly

--- solution/StoryTestExceptionImpl.java
+++ solution/StoryTestExceptionImpl.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package solution;
 import org.junit.Assert;
 import provided.StoryTestException;
@@ -51,58 +50,3 @@ public class StoryTestExceptionImpl extends StoryTestException {
         return numFails;
     }
 }
-
-=======
-package solution;
-import org.junit.Assert;
-import provided.StoryTestException;
-public class StoryTestExceptionImpl extends StoryTestException {
-    String firstFailedSentence;
-    String expected;
-    String result;
-    int numFails;
-
-    public StoryTestExceptionImpl(String sentence, String expected_str, String result_str, int fails)
-    {
-        firstFailedSentence = sentence;
-        expected = expected_str;
-        result = result_str;
-        numFails = fails;
-    }
-    /**
-     * Returns a string representing the sentence
-     * of the first Then sentence that failed
-     */
-    public String getSentance()
-    {
-        return firstFailedSentence;
-    }
-
-    /**
-     * Returns a string representing the expected value from the story
-     * of the first Then sentence that failed.
-     */
-    public String getStoryExpected()
-    {
-        return expected;
-    }
-
-    /**
-     * Returns a string representing the actual value.
-     * of the first Then sentence that failed.
-     */
-    public String getTestResult()
-    {
-        return result;
-    }
-
-    /**
-     * Returns an int representing the number of Then sentences that failed.
-     */
-    public int getNumFail()
-    {
-        return numFails;
-    }
-}
-
->>>>>>> 76464788507bee408bd66a9c8b8afd3ac7517100
